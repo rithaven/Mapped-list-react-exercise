107c409c0857ba5bd6fc108751dcf1e99e8f5f88
rithaven <veneruth4@gmail.com>
2023-03-30 15:10:59 +0200

    add ordered

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ function App() {
   const animals = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse'];
 
   return (
+
     <div className="flex items-center justify-center h-screen">
-      <h2 className="mb-10 text-2xl font-bold">Mapping Through A List And Rendering</h2>
+      
      <ul className="text-3xl list-disc">
+     
       {animals.map((animal) => (
         <li key={animal}>{animal}</li>
       ))}
